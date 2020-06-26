34caa1fd0c0f49e443719b16ff4cf93631e6691c
Clément Bonnet <clement.bonnet16@gmail.com>
2020-06-26 10:46:06 +0200

    handle 404 errors

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ from app import app
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+# handle 404 errors
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
