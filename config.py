e42697b4fb113d0688c15f6c29cd7b9f0cca18f0
Clément Bonnet <clement.bonnet16@gmail.com>
2020-06-26 10:53:31 +0200

    update config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or
-        "blooskyntattoo_by_clement_bonnet_for_clara_tonnele"
+    SECRET_KEY = (os.environ.get("SECRET_KEY") or 
+        "blooskyntattoo_by_clement_bonnet_for_clara_tonnele")
