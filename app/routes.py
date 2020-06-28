a45f7a2d02bfe13a03fee149f3287535b334316c
Clément Bonnet <clement.bonnet16@gmail.com>
2020-06-28 18:08:10 +0200

    404 page better

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,10 @@ from app import app
 def index():
     return render_template("index.html")
 
+@app.route("/about_me")
+def about_me():
+    return render_template("about_me.html")
+
 # handle 404 errors
 @app.errorhandler(404)
 def page_not_found(error):
