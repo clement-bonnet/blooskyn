4df3d19584bd27c6a2f895957b0c87ca68b2e5b7
Clément Bonnet <clement.bonnet16@gmail.com>
2020-07-02 16:17:42 +0200

    translation framework + select language working

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask
 from config import Config
-from flask_babel import Babel
+from flask import Flask, request, session
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 from app import routes
+
+@babel.localeselector
+def get_locale():
+    language = session.get("language", None)
+    if language is None:
+        language = request.accept_languages.best_match(app.config["LANGUAGES"].keys())
+        session["language"] = language
+    return language
